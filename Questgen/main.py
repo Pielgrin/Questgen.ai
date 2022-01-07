987cf3f977aaa9d997269121c39eed5199cfab21
Pielgrin <pierre.pellegrin@viacesi.fr>
2022-01-07 12:19:44 +0100

    update spacy to fr

--- Questgen/main.py
+++ Questgen/main.py
@@ -46,7 +46,7 @@ class QGen:
         # model.eval()
         self.device = device
         self.model = model
-        self.nlp = spacy.load('en_core_web_sm')
+        self.nlp = spacy.load('fr_core_news_sm')
 
         self.s2v = Sense2Vec().from_disk('s2v_old')
 
